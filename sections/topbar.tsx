5fb0314754653c254e964807af42e2006a8b74df
Vasanth <vasanth.selvaraj@mavenberg.com>
2023-08-09 16:19:58 +0530

    Logo name changed

--- sections/topbar.tsx
+++ sections/topbar.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import Logo from "../public/logo.png"
+import Logo from "../public/memoji.png"
 import { useState } from "react";
 
 export default function TopBar() {
