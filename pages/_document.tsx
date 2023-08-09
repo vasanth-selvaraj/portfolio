bb1e2d326e73dfce662b86e6fdfd8b4a595e2bdb
Vasanth <vasanth.selvaraj@mavenberg.com>
2023-08-09 16:37:25 +0530

    Error check

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,7 +5,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <title>Vasanth's Portfolio</title>
+        <title>Vasanth Portfolio</title>
         <meta name="description" content="I am a passionate and creative web developer, specializing in crafting elegant and responsive websites. With a keen eye for design and a deep understanding of user experience, I strive to create engaging and user-friendly digital experiences. Explore my portfolio to see a diverse range of projects that showcase my skills in front-end development, back-end programming, and more." />
 
         <link rel="icon" href="../public/favicon.ico" />
