409e7fc09d5c732452029c763c5c1c66b1ce63e8
Vasanth <vasanth.selvaraj@mavenberg.com>
2023-08-09 16:33:35 +0530

    Added head tag with favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,9 +1,15 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Favicon from "../public/favicon.ico"
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <title>Vasanth's Portfolio</title>
+        <meta name="description" content="I am a passionate and creative web developer, specializing in crafting elegant and responsive websites. With a keen eye for design and a deep understanding of user experience, I strive to create engaging and user-friendly digital experiences. Explore my portfolio to see a diverse range of projects that showcase my skills in front-end development, back-end programming, and more." />
+
+        <link rel="icon" href="../public/favicon.ico" />
+      </Head>
       <body>
         <Main />
         <NextScript />
