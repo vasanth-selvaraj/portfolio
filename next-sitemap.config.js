8dd02f9ec8b439c64ad8ae99cca74973034367e4
Vasanth <vasanth151997@outlook.com>
2023-09-25 18:27:11 +0530

    Sitemap config

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: process.env.SITE_URL || 'https://vasanth-selvaraj.github.io/vasanth',
+    generateRobotsTxt: true, // (optional)
+    // ...other options
+  }
