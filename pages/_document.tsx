49372d152afbfde36cd46bdcdd90fa52d3443123
vasanth-selvaraj97 <vasanth.selvaraj@mavenberg.com>
2023-10-12 23:26:29 +0530

    google search console verification

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
         {/* <title>Vasanth Portfolio</title> */}
         <meta name="description" content="I am a passionate and creative web developer, specializing in crafting elegant and responsive websites. With a keen eye for design and a deep understanding of user experience, I strive to create engaging and user-friendly digital experiences. Explore my portfolio to see a diverse range of projects that showcase my skills in front-end development, back-end programming, and more." />
         <meta property="og:image" content="https://drive.google.com/uc?export=download&id=17qaAtosdLf9lcWWk3rjIKYVeKfJDO8Pp" />
-        <meta name="google-site-verification" content="uc1-2AMqZP0XImtxI0AEPKiMEufy9YnejFU3SWAdayA" />
+        <meta name="google-site-verification" content="x31vTQvK7B2Ral8L2f3C5o3yqslTwke_m8uhpSCeipw" />
         <meta property="og:url" content="https://vasanth.tech" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="https://drive.google.com/uc?export=download&id=1VmcKtY39_jFUtUB_pcSjFj5VYHaPitHJ" />
